82fca439428c6095b5c3c76809556d7d17a8c3f2
Weerachai Plodkaew <s6102041520165@email.kmutnb.ac.th>
2020-06-27 15:26:26 +0700

    created thread

---
+++ thread/main.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+// Let us create a global variable to change it in threads
+int g = 0;
+
+// The function to be executed by all threads
+void *myThreadFun(void *vargp)
+{
+	// Store the value argument passed to this thread
+	int *myid = (int *)vargp;
+
+	// Let us create a static variable to observe its changes
+	static int s = 0;
+
+	// Change static and global variables
+	++s;
+	++g;
+
+	// Print the argument, static and global variables
+	printf("Thread ID: %d, Static: %d, Global: %d\n", *myid, ++s, ++g);
+}
+
+// The function to be executed by all threads
+void *myThreadFun2(void *vargp)
+{
+	// Store the value argument passed to this thread
+	int *myid = (int *)vargp;
+
+	// Let us create a static variable to observe its changes
+	static int s = 0;
+
+	// Change static and global variables
+	++s;
+	++g;
+
+	// Print the argument, static and global variables
+	printf("Thread ID: %d, Static: %d, Global: %d\n", *myid, ++s, ++g);
+}
+
+int main()
+{
+	int i;
+	pthread_t tid1;
+	pthread_t tid2;
+
+	// Let us create three threads
+	for (i = 0; i < 3; i++)
+	{
+		pthread_create(&tid1, NULL, myThreadFun, (void *)&tid1);
+		pthread_create(&tid2, NULL, myThreadFun2, (void *)&tid2);
+	}
+	pthread_join(tid1, NULL);
+
+	pthread_exit(NULL);
+	return 0;
+}
